e3774790be3ef3962de9144e0258dfeebe769b78
Nicodona <njeinicodona@gmail.com>
2021-06-18 09:13:40 -0700

    assig variable number each execution

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - entry point of the proggramm
+ * Description: Finds the last digit of
+ * a random number and undergoes comparison checks
+ * Return: 0 (success)
+ */
+int main(void)
+{
+  int n, lastDigit;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  lastDigit = n % 10;
+  printf("Last digit of %d is %d and is ", n, lastDigit);
+  if (lastDigit > 5)
+    {
+      printf("greater than 5\n");
+    }
+  else if (lastDigit == 0)
+    {
+      printf("0\n");
+    }
+  else
+    {
+      printf("less than 6 and not 0\n");
+    }
+  return (0);
+}
