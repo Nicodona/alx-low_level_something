18a6f4ce9e05f4e5843d196bfab42c533d97b31c
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-18 20:22:16 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - prints alphabets in lowercase and uppercase,
- * followed by a new line
- * Return: Always 0 (Success)
- */
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
-  int i;
-  for (i='z'; i>='a'; i--)
-    {
-      putchar(i);
-    }
-  putchar('\n');
-  return (0);
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
