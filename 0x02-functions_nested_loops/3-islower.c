6df61ea639bc53c5cd3328fdf7083c14abfef20b
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-22 21:45:35 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,11 @@
-islower
+#include "holberton.h"
+/**
+ * _islower - check if the character is lowerCase
+ * @c: the character to be cheked
+ * Return: 1 if lower, 0 if capital
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
