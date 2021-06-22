925305fbd038097b375cf743e001a28a403a32e8
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-22 21:33:35 +0100

    updated

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,21 @@
+ 
 #include "holberton.h"
-int main(void)
-/* main -  prints Holberton, followed by a new line
-* Return: Always 0 (Success)
-*/
-{
-  _putchar("H");
-  _putchar("o");
-  _putchar("l");
-  _putchar("b");
-  _putchar("e");
-  _putchar("r");
-  _putchar("t");
-  _putchar("o");
-  _putchar("n");
-  _putchar("\n");
-  
-return 0;
-}
+/**
+ * main - entry point to the program
+ * Return: 0 (success)
+ */
 
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
