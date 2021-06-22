b83689b24d32e9970845f5fe70bc99aed62163e4
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-22 21:20:28 +0100

    udated hoberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,20 @@
-word
+#include "holberton.h"
+int main(void)
+/* main -  prints Holberton, followed by a new line
+* Return: Always 0 (Success)
+*/
+{
+  _putchar("H");
+  _putchar("o");
+  _putchar("l");
+  _putchar("b");
+  _putchar("e");
+  _putchar("r");
+  _putchar("t");
+  _putchar("o");
+  _putchar("n");
+  _putchar("\n");
+  
+return 0;
+}
+
