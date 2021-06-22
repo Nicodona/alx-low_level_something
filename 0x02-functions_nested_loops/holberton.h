195543f9f6e988f0575b56005e4b86631ab764e1
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-22 21:38:23 +0100

    new commit

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,1 +1,28 @@
-some thing
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar.c */
+int _putchar(char);
+
+/* 1-alphabet.c */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c*/
+void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int c);
+
+/*  4-isalpha.c */
+int _isalpha(int c);
+
+/*  5-sign.c */
+int print_sign(int n);
+
+/* 6-abs.c */
+int _abs(int);
+
+/* 7-print_last_digit.c */
+int print_last_digit(int);
+
+#endif
