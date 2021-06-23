58fbfc755010a9c35c771a02a3a79e82f3c42e22
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-23 18:44:42 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,25 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - entry point
+ * Return: void
  */
+
 void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0;
-
-	while (i < 24)
+	for (i = 0; i < 24; i++)
 	{
-		j = 0;
-		while (j < 60)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+
+		_putchar(i / 10 + 48);
+		_putchar(i % 10 + 48);
+		_putchar(':');
+		_putchar(j / 10 + 48);
+		_putchar(j % 10 + 48);
+		_putchar('\n');
 		}
-		i++;
 	}
 }
