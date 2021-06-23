6dbbf7c1e6d65d5d0dcf2f6eedc37555f7091c20
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-23 14:20:53 +0100

    made a change to this file using betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,25 @@
 #include "holberton.h"
+
 /**
- * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
- * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
- * and resets mins
- * Return: 0
+ * jack_bauer - entry point
+ * Return: void
  */
+
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+	int i, j;
 
-	while (hours <= 23)
+	for (i = 0; i < 24; i++)
 	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
+		for (j = 0; j < 60; j++)
+		{
+
+		_putchar(i / 10 + 48);
+		_putchar(i % 10 + 48);
+		_putchar(':');
+		_putchar(j / 10 + 48);
+		_putchar(j % 10 + 48);
+		_putchar('\n');
+		}
 	}
-	hours++;
-	minutes = 0;
-}
 }
