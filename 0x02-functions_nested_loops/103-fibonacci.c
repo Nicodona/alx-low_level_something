c6a9f51ba70ee01bf532b8687ac9f607323b97de
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-23 19:25:49 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,25 @@
-fibonacci 2
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ * Description: finds the sum of the even-valued
+ * fibonacci numbers below 4million
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+	long t1 = 0, t2 = 1, nextterm = t1 + t2, evenSum = 0;
+
+	for (i = 0; i < 4000000; i++)
+	{
+		if (nextterm % 2 == 0)
+		{
+			evenSum += nextterm;
+		}
+		t1 = t2;
+		t2 = nextterm;
+		nextterm = t1 + t2;
+	}
+	printf("%lu\n", evenSum);
+	return (0);
+}
