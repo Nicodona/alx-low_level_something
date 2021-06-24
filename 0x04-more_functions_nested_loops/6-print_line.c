34181c453fdef5ed1948e3a4d9b32e91b6336bd1
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-24 21:39:45 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,17 @@
  */
 void print_line(int n)
 {
-int a = 0;
+	int a = 0;
 
-if (n > 0)
-{
-while (a <= n)
-{
-_putchar('_');
-a++;
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+	 _putchar('\n');
+	}
+	else
+	_putchar('\n');
 }
-_putchar('\n');
-}
-else
-_putchar('\n');
-}
-
