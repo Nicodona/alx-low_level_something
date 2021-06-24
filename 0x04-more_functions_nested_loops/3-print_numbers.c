d48dca095e581ae16658671f7a7e3f175eb3da27
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-24 20:38:10 +0100

    new change

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,35 +1,19 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are left
- * int the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
-void print_remaining_days(int month, int day, int year)
+void print_numbers(void)
 {
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-{
-if (month >= 3 && day >= 60)
-{
-day++;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
 }
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
-else
-{
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
-}
+
+
