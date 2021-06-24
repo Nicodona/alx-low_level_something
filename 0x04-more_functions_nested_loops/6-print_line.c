714da2334540129a7cc50509cf31a797024d309f
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-24 21:11:06 +0100

    trying to to follow betty complaint

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,18 @@
  */
 void print_line(int n)
 {
-	int a = 0;
+int a = 0;
 
-	if (n > 0)
-	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-	 _putchar('\n');
-	}
-	else
-	_putchar('\n');
+if (n > 0)
+{
+while (a <= n)
+{
+_putchar('_');
+a++;
 }
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
+
