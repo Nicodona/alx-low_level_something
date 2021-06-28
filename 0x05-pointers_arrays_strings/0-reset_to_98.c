4f89294bb2e4116733498110b002fde501c1c1fa
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-06-28 22:27:05 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
-
 /**
- * reset_to_98 - updates the value of the variable
- * the pointer points to to 98
- * @n: pointer to the variable to update
- */
+* reset_to_98 - updates the value of the variable
+* the pointer points to to 98
+* @n: pointer to the variable to update
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
