0243dcaa84a132177d7950ad50c566a8176ecea5
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-07-06 07:45:32 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,53 +1,31 @@
-#include <stdio.h>
 #include "holberton.h"
-/**
- * _strlen - returns the length of a string
- * @str: string
- * Return: return the length
- */
-int _strlen(char *str)
-{
-int length = 0;
 
-while (*str)
-{
-str++;
-length++;
-}
-return (length);
-}
 /**
- * _strncat - conctenates two strings
- * @dest: destination pointer
- * @src: pointer to a string
- * @n: amount to be concatenated
- * Return: concatenated string
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-char *cat  = dest + _strlen(dest);
-int length;
+	int i, j;
 
-if (n > _strlen(src) + _strlen(dest))
-{
-length = _strlen(dest) + _strlen(src);
-}
-else
-{
-length = _strlen(dest) + n;
-}
-while (*src && n > 0)
-{
-*cat += *src;
-src++;
-cat++;
-n--;
-}
-if (n > 0)
-{
-*cat += '\0';
-}
-cat -= (length);
-*dest = *cat;
-return (cat);
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
