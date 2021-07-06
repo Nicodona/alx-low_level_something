45ccf90c2cc3de7deac1819f05c9e467be4ce4fe
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-07-06 07:44:11 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,39 +1,30 @@
 #include "holberton.h"
-/**
- * _strlen - returns the length of a string
- * @str: a string of length to be returned
- * Return: returns the length of a string
- */
-int _strlen(char *str)
-{
-int length = 0;
 
-while (*str)
-{
-str++;
-length++;
-}
-return (length);
-}
 /**
  * _strcat - concatenates two strings
- * @dest: destination pointer
- * @src: pointer to a string
- * Return: concatenated string
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-char *cat = dest + _strlen(dest);
-int length = _strlen(dest) + _strlen(src);
+	int i, j;
 
-while (*src)
-{
-*cat += *src;
-src++;
-cat++;
-}
-*cat += '\0';
-cat -= (length);
-*dest = *cat;
-return (cat);
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
