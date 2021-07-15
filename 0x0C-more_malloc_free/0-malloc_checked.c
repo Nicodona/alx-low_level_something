0f56c6f00bddefbcdb2d98200662e84eac543b33
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-07-15 21:23:59 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,30 @@
-#include <stdlib.h>
+#include "holberton.h"
 #include <stdio.h>
-/**
-* malloc_checked - allocates memory using malloc
-* @b: size to be allocated
-* Return: pointer to allocated memory or exit with 98
-**/
-void *malloc_checked(unsigned int b)
-{
-void *p;
+#include <stdlib.h>
 
-p = malloc(b);
-if (p == NULL)
-exit(98);
-return (p);
+/**
+ * create_array - creates array of chars
+ * @size: size of the allocatd memory
+ * @c: initializing character
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * size);
+	if (!size || !ptr)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+	}
+	return (ptr);
 }
