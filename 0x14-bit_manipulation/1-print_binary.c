01ad39b3c13e986c88cc0ec5885845191b3d7feb
njei nicodona <njeinicodona@gmail.com>
2021-08-06 20:05:54 +0100

    this is a new lesson

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1-print_binary.c
+ * Author: KIGARDE THOMAS
+ */
+#include "main.h"
+
+/**
+ * print_binary - Prints binary representation of number
+ * @n: number to be printed in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
